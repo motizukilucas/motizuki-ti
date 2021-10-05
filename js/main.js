6a66df868e3b093f8616a014956df669f8f5b12f
Lucas Motizuki <motizuki@protonmail.com>
2021-10-04 21:24:50 -0300

    Add count animation

--- js/main.js
+++ js/main.js
@@ -39,3 +39,16 @@ $(document).ready(function(){
     map: map,
     });
 });
+
+// Count effect
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 10000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
