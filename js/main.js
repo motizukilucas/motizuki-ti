3eae850f64106bb7912f525b622a70f26fd3c211
Lucas Motizuki <motizuki@protonmail.com>
2021-10-04 21:27:44 -0300

    Update count animation time

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ $('.count').each(function () {
     $(this).prop('Counter',0).animate({
         Counter: $(this).text()
     }, {
-        duration: 10000,
+        duration: 20000,
         easing: 'swing',
         step: function (now) {
             $(this).text(Math.ceil(now));
