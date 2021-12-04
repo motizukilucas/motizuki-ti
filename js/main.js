0c87809683e4f85c9bfd9ca9d42fb47ba06c131b
Lucas Motizuki <motizuki@protonmail.com>
2021-12-04 10:51:48 -0300

    Fix count rollback bug

--- js/main.js
+++ js/main.js
@@ -53,17 +53,32 @@ $(document).ready(function() {
 
 // Count effect
 $(window).scroll(function () {
-    $('.count').each(function () {
-        $(this).prop('Counter',0).animate({
-            Counter: $(this).text()
-        }, {
-            duration: 10000,
-            easing: 'swing',
-            step: function (now) {
-                $(this).text(Math.ceil(now));
-            }
-        });
-    });
+    if ($('.counter-wrapper').isInViewport()) {
+
+        $('.count').each(function() {
+            var $this = $(this),
+                countTo = $this.attr('data-count');
+          
+            $({ countNum: $this.text()}).animate({
+              countNum: countTo
+            },
+          
+            {
+          
+              duration: 3000,
+              easing:'linear',
+              step: function() {
+                $this.text(Math.floor(this.countNum));
+              },
+              complete: function() {
+                $this.text(this.countNum);
+                //alert('finished');
+              }
+            });
+          });
+        
+
+    }
 });
 
 // Email form
